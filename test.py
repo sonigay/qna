d7bfe327170452fb39ba328cb9b0d136e9b77f0e
sonigay <43840048+sonigay@users.noreply.github.com>
2020-02-01 21:28:53 +0900

    Update test.py

--- test.py
+++ test.py
@@ -7,8 +7,7 @@ from gensim.models import doc2vec
 from gensim.models.doc2vec import TaggedDocument
 import pandas as pd
 import jpype
-import multiprocessing
-from konlpy.tag import twitter_korean
+from konlpy.tag import Kkma
 
 
 
@@ -19,17 +18,18 @@ faqs = pd.read_csv(('test.csv'), encoding='CP949')
 
 
 
-twitter_korean = twitter_korean()
-filter_twitter_korean = ['NNG',  #보통명사
+kkma = Kkma()
+filter_kkma = ['NNG',  #보통명사
 	       'NNP',  #고유명사
 	       'OL' ,  #외국어
 	       ]
 
 def tokenize_kkma(doc):
-	token_doc = ['/'.join(word) for word in twitter_korean.pos(doc)]
+	jpype.attachThreadToJVM()
+	token_doc = ['/'.join(word) for word in kkma.pos(doc)]
 	return token_doc
 
-print(twitter_korean.pos("히라마블로그에온걸환영해!"))
+print(Kkma.pos("히라마블로그에온걸환영해!"))
 
 
 
